8e852090390a67783bd2c939ac7d9f2df9909fe8
TimurIskandarov <tim-darov@rambler.ru>
2020-03-23 01:20:17 +0400

    feat: add createDreamTeam function

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,11 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if(!Array.isArray(members)) return false;
+  let name = '';
+  for(let member of members) {
+    if(typeof member == "string") {
+      member = member.trim();
+      name += member[0].toUpperCase();
+    }
+  }
+  return name.split('').sort().join('');
 };
